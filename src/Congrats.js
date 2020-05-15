0570f4a11d4556543746697c1e5b4a2cd2b7aec7
vibecode <rumashkin@gmail.com>
2020-05-15 17:37:45 +0200

    test congrats

---
+++ src/Congrats.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Congrats() {
+  return <div></div>
+}
+
+export default Congrats
---
+++ src/Congrats.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import Congrats from './Congrats'
+import { findByTestAttr } from './testUtils'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+describe('Congrats', () => {
+  const wrapper = shallow(<Congrats />)
+  const component = findByTestAttr(wrapper, 'component-congrats')
+
+  it('renders without error', () => {
+    expect(component.length).toBe(1)
+  })
+
+  it('renders no text when success prop is false', () => {
+    expect(component.text()).toBe('')
+  })
+
+  it('renders no empty congrats when success prop is true', () => {
+    wrapper.setProps({ success: true })
+    const msg = wrapper.findByTestAttr('congrats-message')
+    expect(msg.text().length).not.toBe(0)
+  })
+})
