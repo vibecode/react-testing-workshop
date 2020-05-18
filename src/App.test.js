84abc0a4764158dcbfcd97ab8674bcec056a17aa
vibecode <rumashkin@gmail.com>
2020-05-18 18:04:35 +0200

    move enzyme adapter setup to config

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import App from './App'
-import { configure } from 'enzyme'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
+import { shallow } from 'enzyme'
 
 const wrap = (props = {}, state = null) => {
   return shallow(<App {...props} />)
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
 import Congrats from './Congrats'
 import { findByTestAttr } from './testUtils'
-
-Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 describe('Congrats', () => {
   const wrapper = shallow(<Congrats />)
